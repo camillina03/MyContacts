914e5c51ce1c3b38b72a681d157f5b86ff69f89d
camillina03 <camillinabianchi@gmail.com>
2023-12-14 00:09:07 +0100

    Aggiungere i file di progetto.

---
+++ Connections/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MyContacts.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyContacts.Connections
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Contatto> Contatti { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
+
+
---
+++ Entities/Contatto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyContacts.Entities
+{
+    public class Contatto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Cognome { get; set; }
+
+        [Required]
+        public string Sesso { get; set; }
+
+        public DateOnly? DataNascita { get; set; }
+
+
+        public string NumeroTelefono { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Citta { get; set; }
+    }
+}
